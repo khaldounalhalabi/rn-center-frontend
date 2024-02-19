ce1d719f77dfc8aa63a1748a9271914b3cedeb34
mohammad606 <59096227+mohammad606@users.noreply.github.com>
2024-02-19 23:55:36 +0200

    remove context and add close button

--- src/hooks/OpenSearchForm.tsx
+++
@@ -1,8 +0,0 @@
-import React, { createContext } from "react";
-
-interface OpenMenuContextType {
-    open: boolean;
-    setOpen: (value: boolean) => void;
-}
-
-export const OpenSearchContext = createContext<OpenMenuContextType | null>(null);

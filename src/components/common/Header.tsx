517e947daea7b1b4cf09c94419f4a4f3abbb7b27
khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-02-20 21:27:04 +0000

    Revert "Merge branch 'navbar' into 'dev'"
    This reverts merge request !2

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,17 +1,24 @@
 "use client";
 import Sidebar from "@/components/common/Sidebar/Sidebar";
-import Navbar from "@/components/common/NavBar/Navbar";
-import { useState} from "react";
-import {OpenMenuContext} from '@/hooks/OpenMenu'
+import Navbar from "@/components/common/Navbar";
+import { createContext, Dispatch, SetStateAction, useState } from "react";
 
+interface OpenMenuContextType {
+  openMenu?: boolean;
+  setOpenMenu?: (prevState: boolean) => Dispatch<SetStateAction<boolean>>;
+}
+
+export const OpenMenuContext: React.Context<OpenMenuContextType | undefined> =
+  createContext<OpenMenuContextType | undefined>(undefined);
 
 export const Header = () => {
-
-    const [open, setOpen] = useState<boolean>(false);
+  const [openMenu, setOpenMenu] = useState(false);
   return (
     <header className={`md:grid md:grid-cols-4 max-h-20`}>
-        <Sidebar open={open} setOpen={setOpen}/>
-        <Navbar open={open} setOpen={setOpen} />
+      <OpenMenuContext.Provider value={{ openMenu, setOpenMenu }}>
+        <Sidebar />
+        <Navbar />
+      </OpenMenuContext.Provider>
     </header>
   );
 };

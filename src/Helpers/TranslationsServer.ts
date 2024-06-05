67405243206828c689b2d69a17ab1bd9dc7898b6
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-06-05 21:38:48 +0300

    fixes : updates

--- src/Helpers/TranslationsServer.ts
+++ src/Helpers/TranslationsServer.ts
@@ -1,25 +1,30 @@
-'use server'
+"use server";
 import { getCookieServer } from "@/Actions/serverCookies";
 
+const TranslateServer = async (
+  val: string | undefined | null,
+  object?: boolean,
+) => {
+  if (!val && object) {
+    return {
+      en: "",
+      ar: "",
+    };
+  }
 
-const TranslateServer =async (val: string | undefined | null, object?: boolean)=>{
-    const locale = await getCookieServer("NEXT_LOCALE") ?? "en"
-    const tr = JSON.parse(val ?? "{}");
-    if(!val && object){
-        return {
-            en:"",
-            ar:""
-        }
-    }else if(!val && !object){
-        return ""
-    }else if (val && !object){
-        if(locale == "en"){
-            return tr.en
-        }else {
-            return tr.ar
-        }
-    }else if(val && object){
-        return tr
+  if (!val && !object) {
+    return "";
+  }
+  const locale = (await getCookieServer("NEXT_LOCALE")) ?? "en";
+  const tr = JSON.parse(val ?? "{}");
+  if (val && !object) {
+    if (locale == "en") {
+      return tr.en;
+    } else {
+      return tr.ar;
     }
-}
-export default TranslateServer
+  } else if (val && object) {
+    return tr;
+  }
+};
+export default TranslateServer;

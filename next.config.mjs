d0d9891e50400fa1337741176c8c22d0b6418101
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-08-31 02:18:22 +0300

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,31 @@
 /** @type {import('next').NextConfig} */
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 import withPWAInit from "@ducanh2912/next-pwa";
+
 const withNextIntl = createNextIntlPlugin();
 const withPWA = withPWAInit({
-    dest: "public",
-    register: true,
-    swSrc: 'service-worker.js',
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swcMinify: true,
-    disable: false,
-    workboxOptions: {
-        disableDevLogs: true,
-    }
+  dest: "public",
+  register: true,
+  swSrc: "service-worker.js",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+    exclude: [/middleware-manifest.json$/],
+    excludeChunks: [/middleware-manifest.json$/],
+  },
+  publicExcludes: [/middleware-manifest.json$/],
 });
 const nextConfig = {
-    env: {
-        // localApi: 'http://localhost/pom/public/api/',
-        localApi: 'https://api.planetofmedicine.com/api/',
-        firebase_key: "BIutuhaOvqImTR8RpGVoDLHDSzeJay1fAXWes5wWtLmLLBKkyOxUebJA2fQu3hfiwhHq51BKfzDT-tni6ndtVcM",
-    },
+  env: {
+    // localApi: 'http://localhost/pom/public/api/',
+    localApi: "https://api.planetofmedicine.com/api/",
+    firebase_key:
+      "BIutuhaOvqImTR8RpGVoDLHDSzeJay1fAXWes5wWtLmLLBKkyOxUebJA2fQu3hfiwhHq51BKfzDT-tni6ndtVcM",
+  },
 };
 
 export default withNextIntl(withPWA(nextConfig));

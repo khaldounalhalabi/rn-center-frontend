0ed47b98777dcae8cbb6af40dbb474d9e1a2a55e
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-10-17 01:57:15 +0300

    fixes

--- src/app/[locale]/admin/join-requests/[joinRequestId]/page.tsx
+++ src/app/[locale]/admin/join-requests/[joinRequestId]/page.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import { getTranslations } from "next-intl/server";
 import { JoinRequestService } from "@/services/JoinRequestService";
 import Grid from "@/components/common/ui/Grid";
-import {LabelValue} from "@/components/common/ui/LabelsValues/LabelValue";
+import { LabelValue } from "@/components/common/ui/LabelsValues/LabelValue";
+import TranslateServer from "@/Helpers/TranslationsServer";
 
 const Page = async ({
   params: { joinRequestId },
@@ -17,13 +18,31 @@ const Page = async ({
   return (
     <PageCard>
       <div className="flex justify-between items-center w-full h-24">
-        <h2 className="card-title">{t("join_request")} : {joinRequest.doctor_name}</h2>
+        <h2 className="card-title">
+          {t("join_request")} : {joinRequest.doctor_name}
+        </h2>
       </div>
       <Grid>
-        <LabelValue label={t("doctor_name")} value={joinRequest?.doctor_name} color={"warning"}/>
-        <LabelValue label={t("clinic_name")} value={joinRequest?.clinic_name} color={"error"}/>
-        <LabelValue label={t("phone")} value={joinRequest?.phone_number} color={"info"}/>
-        <LabelValue label={t("city")} value={joinRequest?.city?.name} color={"accent"}/>
+        <LabelValue
+          label={t("doctor_name")}
+          value={joinRequest?.doctor_name}
+          color={"warning"}
+        />
+        <LabelValue
+          label={t("clinic_name")}
+          value={joinRequest?.clinic_name}
+          color={"error"}
+        />
+        <LabelValue
+          label={t("phone")}
+          value={joinRequest?.phone_number}
+          color={"info"}
+        />
+        <LabelValue
+          label={t("city")}
+          value={await TranslateServer(joinRequest?.city?.name)}
+          color={"accent"}
+        />
       </Grid>
     </PageCard>
   );

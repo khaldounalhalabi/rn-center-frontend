96cb29c1e61ec1eb05b612e2c09bfad9672a096f
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-11-17 19:00:07 +0300

    fixes

--- src/components/landing/CheckUserImg.tsx
+++ src/components/landing/CheckUserImg.tsx
@@ -7,15 +7,13 @@ import RoundedImage from "@/components/common/RoundedImage";
 const CheckUserImg = () => {
   const user = HandleGetUserData();
   return (
-    <>
-      <Link className={"w-7 h-7 mx-2"} href={"/doctor"}>
-        {user && user?.image?.[0]?.file_url ? (
-          <RoundedImage src={user?.image?.[0]?.file_url} alt={"user-profile"} />
-        ) : (
-          <UserIcon className={"w-6 h-6 fill-[#013567]  cursor-pointer"} />
-        )}
-      </Link>
-    </>
+    <Link className={"w-7 h-7 mx-2"} href={"/doctor"}>
+      {user && user?.image?.[0]?.file_url ? (
+        <RoundedImage src={user?.image?.[0]?.file_url} alt={"user-profile"} />
+      ) : (
+        <UserIcon className={"w-6 h-6 fill-[#013567]  cursor-pointer"} />
+      )}
+    </Link>
   );
 };
 

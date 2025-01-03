a0f3118c26674fc8f15c10ec6a8d5c419a9e46f2
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-01-03 23:10:56 +0300

    fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "pom-front-dev",
+      name: "pom-front",
       script: "npm",
       args: "start",
       env: {
         NODE_ENV: "production",
       },
-      instances: 1,
+      instances: 2,
       watch: false,
-      output: "./../../pom-front-dev-logs/out.log",
-      error: "./../../pom-front-dev-logs/error.log",
+      output: "./../../pom-front-logs/out.log",
+      error: "./../../pom-front-logs/error.log",
       log_date_format: "YYYY-MM-DD HH:mm Z",
     },
   ],

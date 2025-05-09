04627d0ab952a58746d0fc01167d38116e2c989a
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-05-09 17:19:55 +0300

    Cleaning: remove unwanted services and models

---
+++ src/services/ServiceService.ts
@@ -0,0 +1,35 @@
+import { BaseService } from "@/services/BaseService";
+import { Service } from "@/Models/Service";
+import { ApiResponse } from "@/Http/Response";
+import { GET } from "@/Http/Http";
+
+export class ServiceService extends BaseService<ServiceService, Service>() {
+  public getBaseUrl(): string {
+    return `${this.role}/services`;
+  }
+
+  public async getClinicService(
+    clinicId: number | undefined,
+    page: number = 0,
+    search?: string,
+    sortCol?: string,
+    sortDir?: string,
+    per_page?: number,
+    headers?: Record<string, any>,
+    params?: object,
+  ): Promise<ApiResponse<Service[]>> {
+    const res = await GET<Service[]>(
+      `${this.role}/clinics/${clinicId}/services`,
+      {
+        page: page,
+        search: search,
+        sort_col: sortCol,
+        sort_dir: sortDir,
+        per_page: per_page,
+        ...params,
+      },
+      headers,
+    );
+    return await this.errorHandler(res);
+  }
+}

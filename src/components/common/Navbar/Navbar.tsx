3b7f7c1c7877e64c4356cd242b411fcd557805cf
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-05-10 22:54:46 +0300

    Cleaning: remove Searchbar.tsx component and its usage

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,11 +1,8 @@
 "use client";
-import React, { useState } from "react";
-import SearchIcon from "@/components/icons/SearchIcon";
+import React from "react";
 import MenuIcon from "@/components/icons/MenuIcon";
-import Searchbar from "@/components/common/Navbar/Searchbar";
 import NotificationsPopover from "@/components/common/Navbar/NotificationsPopover";
 import LanguagePopover from "@/components/common/Navbar/languagePopover";
-import OpenAndClose from "@/hooks/OpenAndClose";
 import { useRouter } from "next/navigation";
 import { usePathname } from "@/navigation";
 import ArrowLeft from "@/components/icons/ArrowLeft";
@@ -24,22 +21,12 @@ const Navbar = ({
     md: boolean;
   }>;
 }) => {
-  const [showSearchForm, setShowSearchForm] = useState<boolean>(false);
   const rout = useRouter();
   const path = usePathname();
   return (
     <nav
       className={`w-full h-16 relative bg-white shadow-md flex justify-between max-h-20 px-2 items-center py-4 col-span-4 lg:col-span-3`}
     >
-      <div
-        className={
-          showSearchForm
-            ? "w-full h-16 lg:w-full md:right-0 lg:left-auto left-0  absolute z-30 top-0 translate-y-0 ease-in-out duration-200"
-            : "w-full h-0 absolute overflow-clip translate-y-[-200px] ease-in-out duration-300"
-        }
-      >
-        <Searchbar setShowSearchForm={setShowSearchForm} />
-      </div>
       <div className={`flex w-[inherit] justify-start gap-3 items-center`}>
         <MenuIcon
           className={`h-6 w-6 cursor-pointer lg:hidden`}
@@ -53,13 +40,6 @@ const Navbar = ({
         >
           <ArrowLeft className={"w-5 h-5"} />
         </button>
-
-        <SearchIcon
-          className={`h-6 w-6 cursor-pointer hidden`}
-          onClick={() => {
-            OpenAndClose(showSearchForm, setShowSearchForm);
-          }}
-        />
       </div>
       <div className={`flex justify-between items-center gap-2 `}>
         <LanguagePopover />

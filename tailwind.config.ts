905b5b982cbcac1d562b9e3e0139c8d60c6bf9bf
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-05-12 22:53:47 +0300

    Enhancement: install shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,62 +3,111 @@ import type { Config } from "tailwindcss";
 const plugin = require("tailwindcss/plugin");
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/common/ui/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("tailwindcss-animate")],
   daisyui: {
     themes: ["light"],
   },
   theme: {
-    letterSpacing: {
-      "1": "0em",
-      "2": "0.025em",
-      "3": "0.05em",
-      "4": "0.1em",
-      "5": "0.15em",
-      "6": "0.2em",
-      "7": "0.25em",
-    },
-    extend: {
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-      },
-      animation: {
-        marquee: "marquee 15s linear infinite",
-      },
-      colors: {
-        pom: "#1fb8b9",
-        title: "#151D48",
-        "brand-primary": "#1FB8B9",
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-    safelist: [
-      {
-        pattern: /^grid-cols-\d+$/,
-        variants: [
-          "lg",
-          "hover",
-          "focus",
-          "lg:hover",
-          "md",
-          "sm",
-          "md:hover",
-          "sm:hover",
-        ],
-      },
-    ],
+  	letterSpacing: {
+  		'1': '0em',
+  		'2': '0.025em',
+  		'3': '0.05em',
+  		'4': '0.1em',
+  		'5': '0.15em',
+  		'6': '0.2em',
+  		'7': '0.25em'
+  	},
+  	extend: {
+  		keyframes: {
+  			marquee: {
+  				'0%': {
+  					transform: 'translateX(0%)'
+  				},
+  				'100%': {
+  					transform: 'translateX(-100%)'
+  				}
+  			}
+  		},
+  		animation: {
+  			marquee: 'marquee 15s linear infinite'
+  		},
+  		colors: {
+  			pom: '#1fb8b9',
+  			title: '#151D48',
+  			'brand-primary': '#1FB8B9',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	},
+  	safelist: [
+  		{
+  			pattern: '/^grid-cols-\\d+$/',
+  			variants: [
+  				'lg',
+  				'hover',
+  				'focus',
+  				'lg:hover',
+  				'md',
+  				'sm',
+  				'md:hover',
+  				'sm:hover'
+  			]
+  		}
+  	]
   },
 };
 export default config;

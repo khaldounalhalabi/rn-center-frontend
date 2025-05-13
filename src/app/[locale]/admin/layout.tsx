b6905828c9d614dc13d07720b73cdc6952f46ba2
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-05-13 22:19:01 +0300

    Enhancement: adjust the whole layout

--- src/app/[locale]/admin/layout.tsx
+++ src/app/[locale]/admin/layout.tsx
@@ -11,42 +11,34 @@ const Layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <div>
-      <main
-        style={{
-          background:
-            "linear-gradient(to bottom, rgba(249, 250, 251, 0.9), rgba(249, 250, 251, 0.9)), url(https://dc621.4shared.com/img/GqP7JQWBjq/s24/18e1e7686a0/overlay_4?async&rand=0.9085352286261172)",
-        }}
-        className={`h-fit min-h-[calc(100vh-4rem)] w-full`}
+    <main>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <AllProviders>
-            <div
-              className={`col-span-4 col-start-1 h-screen w-full overflow-y-scroll md:col-span-4 md:col-start-2`}
-            >
-              <SidebarProvider>
-                <AppSidebar variant="inset" />
-                <SidebarInset>
-                  <SiteHeader />
-                  <div className="flex flex-1 flex-col">
-                    <div className="@container/main flex flex-1 flex-col gap-2">
-                      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                        {children}
-                      </div>
+        <AllProviders>
+          <div
+            className={`col-span-4 col-start-1 h-screen w-full overflow-y-scroll md:col-span-4 md:col-start-2`}
+          >
+            <SidebarProvider>
+              <AppSidebar variant="inset" />
+              <SidebarInset>
+                <SiteHeader />
+                <div className="flex flex-1 flex-col">
+                  <div className="@container/main flex flex-1 flex-col gap-2">
+                    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                      {children}
                     </div>
                   </div>
-                </SidebarInset>
-              </SidebarProvider>
-            </div>
-          </AllProviders>
-        </ThemeProvider>
-      </main>
-    </div>
+                </div>
+              </SidebarInset>
+            </SidebarProvider>
+          </div>
+        </AllProviders>
+      </ThemeProvider>
+    </main>
   );
 };
 
--- src/components/ui/shadcn/site-header.tsx
+++ src/components/ui/shadcn/site-header.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "@/components/ui/shadcn/mode-toggle";
 
 export function SiteHeader() {
   return (
-    <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
+    <header className="sticky top-0 z-10 bg-background group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
       <div className={"flex w-full items-center justify-between"}>
         <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
           <SidebarTrigger className="-ml-1" />
@@ -14,7 +14,7 @@ export function SiteHeader() {
           />
           <h1 className="text-base font-medium">Admin Panel</h1>
         </div>
-        <div className={"flex items-center justify-end"}>
+        <div className={"flex items-center justify-end px-2"}>
           <ModeToggle />
         </div>
       </div>

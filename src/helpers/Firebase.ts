7fbc68a7e9f7151151dbe6a820a57fe8d8b07547
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-05-29 22:50:30 +0300

    Enhancement: adjust firebase config

--- src/helpers/Firebase.ts
+++ src/helpers/Firebase.ts
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRdZ92EFVoKXsHyJrM3etgDvtRDUxMB7U",
-  authDomain: "planetofmedicine-6cb52.firebaseapp.com",
-  projectId: "planetofmedicine-6cb52",
-  storageBucket: "planetofmedicine-6cb52.appspot.com",
-  messagingSenderId: "611001564211",
-  appId: "1:611001564211:web:2e3578e4438e13b2543746",
+  apiKey: "AIzaSyC-HXOnDLprjYVyUWxN3oWMbywkJCNJtqA",
+  authDomain: "astral-host-372216.firebaseapp.com",
+  projectId: "astral-host-372216",
+  storageBucket: "astral-host-372216.firebasestorage.app",
+  messagingSenderId: "55453435164",
+  appId: "1:55453435164:web:2e3a85ec8028a2f467e6d9"
 };
 
 // Initialize Firebase

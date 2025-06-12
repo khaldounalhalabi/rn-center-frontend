c3090a932ec32c9ac07f9eed8343b44215b88115
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-06-13 01:00:10 +0300

    Refactor layout and enhance user hook error handling
    Removed unused imports from the admin layout file to streamline the code. Enhanced the useUser hook to throw an error when used outside of the UserProvider, improving error handling and ensuring proper context usage. These changes contribute to cleaner code and better user context management.

--- src/hooks/UserHook.ts
+++ src/hooks/UserHook.ts
@@ -3,6 +3,11 @@ import { useContext } from "react";
 
 const useUser = () => {
   const context = useContext(UserContext);
+
+  if (!context) {
+    throw "Cannot Use User Hook Outside Of User Provider";
+  }
+
   return {
     user: context?.user,
     setUser: context?.setUser,

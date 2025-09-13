d386a2afd161f209837b77dab92ec7aa95981a7c
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-09-13 16:21:50 +0300

    Add README.md file

---
+++ README.md
@@ -0,0 +1,35 @@
+# Reslan Alnaal Medical Center Management System (Graduation Project)
+
+## Introduction
+This is the frontend of the project made to seize the bachelor's degree in software engineering at Damascus University.
+The project is a medical center management system which provides a platform for the management of the center's staff and patients.
+
+## Prerequisites
+- docker
+- MySQL
+- Git
+- Orthanc Server
+- Node.js
+- OHIF fork from https://github.com/khaldounalhalabi/OHIF-Fork
+
+## Technologies
+- PHP/Laravel
+- MySQL
+- ANTLR 4
+- Next.js
+- Tailwind CSS
+- Shadcn
+- React Native
+- Expo
+
+## Installation and running
+1. run `npm install`
+2. run `npm run dev`
+
+now you can access the backend at http://localhost:9000
+
+## Next Steps
+- Clone And Run The Backend Project [Frontend Repository URL](https://github.com/khaldounalhalabi/rn-center-backend)
+- Clone And Run The OHIF Fork [OHIF Fork Repository URL](https://github.com/khaldounalhalabi/OHIF-Fork)
+- Clone And Run The Employee Mobile App [Employee Mobile App Repository URL](https://github.com/khaldounalhalabi/rn-center-employee-mobile-app)
+- Clone And Run The Patient Mobile App [Patient Mobile App Repository URL](https://github.com/khaldounalhalabi/rn-center-patient-mobile-app)
--- next.config.mjs
+++ next.config.mjs
@@ -4,39 +4,32 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withNextIntl = createNextIntlPlugin();
 const withPWA = withPWAInit({
-  dest: "public",
-  register: true,
-  swSrc: "service-worker.js",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: false,
-  workboxOptions: {
-    disableDevLogs: true,
-    exclude: [/middleware-manifest.json$/],
-  },
+    dest: "public",
+    register: true,
+    swSrc: "service-worker.js",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: false,
+    workboxOptions: {
+        disableDevLogs: true,
+        exclude: [/middleware-manifest.json$/],
+    },
 });
 const nextConfig = {
-  env: {
-    localApi: "http://localhost/rn-center-backend/public/api/",
-    // localApi: "https://rn.cubeta.io/api/",
-    firebase_key: "",
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        pathname: "/rn-center-backend/public/**",
-      },
-      {
-        protocol: "https",
-        hostname: "rn.cubeta.io",
-        pathname: "/**",
-      },
-    ],
-  },
+    env: {
+        localApi: "http://localhost/rn-center-backend/public/api/",
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+                pathname: "/rn-center-backend/public/**",
+            }
+        ],
+    },
 };
 
 export default withNextIntl(nextConfig);
